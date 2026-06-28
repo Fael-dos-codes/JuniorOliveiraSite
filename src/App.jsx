62a69febdaffa5ab8e9d39f70fbcf2edd2af8865
Fael dos Codes <rafaelfe0798@gmail.com>
2026-06-28 19:54:48 -0300

    Configure App.jsx for Landing Page (Linktree) only

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
 import WebsiteHome from './pages/WebsiteHome';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Site Institucional Principal */}
-        <Route path="/" element={<WebsiteHome />} />
-        
-        {/* Landing Page (Focada em conversão/tráfego) */}
-        <Route path="/campanha" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <WebsiteHome />;
 }
